6496bf479fc3c09b3f0527ba09bf08f6fd1a45bf
valentinilas <valentinilas@gmail.com>
2019-11-19 19:16:37 +0100

    fixed closest with polyfill

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                              Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+
 // slick slider
 var slickOptions = {
   dots: false,
